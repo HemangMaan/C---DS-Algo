086d1f4fc48b045f0211b7565270af520cb05fae
HemangMaan <hemangmaan@gmail.com>
2021-02-20 18:31:35 +0530

    UniquePaths#62

---
+++ DynamicProgramming/UniquePaths#62.cpp
@@ -0,0 +1,38 @@
+// A robot is located at the top-left corner of a m x n grid.
+// The robot can only move either down or right at any point in time. 
+// The robot is trying to reach the bottom-right corner of the grid.
+// How many possible unique paths are there?
+
+// Input: m = 3, n = 2
+// Output: 3
+// Explanation:
+// From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+// 1. Right -> Down -> Down
+// 2. Down -> Down -> Right
+// 3. Down -> Right -> Down
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int uniquePaths(int m, int n) {
+    int dp[m][n];
+    for(int i=0;i<m;i++){
+        dp[i][0]=1;
+    }
+    for(int i=0;i<n;i++){
+        dp[0][i]=1;
+    }
+    for(int i=1;i<m;i++){
+        for(int j=1;j<n;j++){
+            dp[i][j]=dp[i-1][j] + dp[i][j-1];
+        }
+    }
+    return dp[m-1][n-1];
+}
+
+int main(){
+    int n,m;
+    cin>>m>>n;
+    int paths = uniquePaths(m,n);
+    cout<<paths<<endl;
+}
